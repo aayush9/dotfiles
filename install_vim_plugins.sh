337141ba87b53c22a5b79ade72296a2db57b557b
Aayush Aggarwal <aayush9@Aayushs-MacBook-Air.local>
2021-07-18 21:42:53 +0530

    Add vim plugins using vim packages

---
+++ install_vim_plugins.sh
@@ -0,0 +1,11 @@
+pushd
+
+mkdir -p  ~/.vim/pack/bundle/start
+cd ~/.vim/pack/bundle/start
+
+git clone https://github.com/preservim/nerdtree.git
+git clone https://github.com/jiangmiao/auto-pairs.git
+git clone https://github.com/vim-airline/vim-airline.git
+git clone https://github.com/ycm-core/YouCompleteMe.git
+
+popd
