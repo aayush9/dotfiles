79021d74adfbbe2ed658390c92954ea2ee722c90
Aayush Aggarwal <aayushaggarwal123@yahoo.co.in>
2022-04-14 02:58:30 +0530

    Changes to powershell config

--- oh-my-posh.ps1
+++ oh-my-posh.ps1
@@ -1,10 +1,8 @@
 #Requires -RunAsAdministrator
 
-winget install JanDeDobbeleer.OhMyPosh
-
 cp "Meslo LG M Regular Nerd Font Complete.ttf" $env:systemroot\fonts -Force
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v "FontName (TrueType)" /t REG_SZ /d "Meslo LG M Regular Nerd Font Complete.ttf" /f
 
-Install-Module DirColors -Force
+Install-Module oh-my-posh,Terminal-Icons,DirColors -Force
 
 cp Windows_PowerShell_profile.ps1 $PROFILE -Force
