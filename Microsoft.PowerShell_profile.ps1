94d6054498c5d453dd466989e7e8c6223cfce63e
Aayush Aggarwal <aayushaggarwal123@yahoo.co.in>
2022-04-14 09:20:21 +0530

    Powershell profile changes

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,26 +1,27 @@
 $global:DefaultUser = [System.Environment]::UserName
 
-function touch {
-  set-content -Path ($args[0]) -Value ($null)
-}
-
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadlineKeyHandler -Chord Tab -Function MenuComplete
-
-Import-Module DirColors
 
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
 
-if ($env:WT_SESSION) {
-  oh-my-posh --init --shell pwsh --config ~\dotfiles\.agnoster.omp.json | Invoke-Expression
-} 
+if ($env:WT_SESSION -or ($env:TERM_PROGRAM -eq 'vscode')) {
+  Import-Module -Name oh-my-posh,DirColors,Terminal-Icons
+  Set-PoshPrompt -Theme ~\dotfiles\PSTheme.omp.json
+}
+
+function touch {
+  set-content -Path ($args[0]) -Value ($null)
+}
 
 function which($name) {
   Get-Command $name | Select-Object -ExpandProperty Definition
 }
 
 New-Alias vi vim
+
+Write-Host $NULL
